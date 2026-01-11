16e468e4f2c36a414b5c94dafc2334385eaa29cf
thara-messeroux <tharamesseroux@gmail.com>
2026-01-11 04:48:48 -0500

    define the required variables used to track the state of the game.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,16 @@ console.log("App initialized");
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+// a.Use a variable named board to represent the state of the squares on the board.
 
+// b.Use a variable named turn to track whose turn it is.
+
+// c.Use a variable named winner to represent if anyone has won yet.
+
+// d.Use a variable named tie to represent if the game has ended in a tie.
+
+
+let board, turn, winner,tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -20,4 +29,18 @@ console.log("App initialized");
 
 
 
+//1) Define the required variables used to track the state of the game.
+
+//2) Store cached element references.
+
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality.
 
